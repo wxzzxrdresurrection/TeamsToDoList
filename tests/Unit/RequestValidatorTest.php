badceff9aa662b237bbe3c0d3e4a8b6f60d36bcb
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-12-05 01:44:57 -0600

    test(app): enhance RequestValidator tests with user and email validation cases

--- tests/Unit/RequestValidatorTest.php
+++ tests/Unit/RequestValidatorTest.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class RequestValidatorTest extends TestCase
 {
-    public function testSuccessfullValidation(): void
+    public function testSuccessfulTeamlValidation(): void
     {
         $request = new Request();
         $request->replace([
@@ -24,7 +24,7 @@ class RequestValidatorTest extends TestCase
         $this->assertEmpty($validator);
     }
 
-    public function testFailedValidation(): void
+    public function testFailedTeamValidation(): void
     {
         $request = new Request();
         $request->replace([
@@ -39,4 +39,175 @@ class RequestValidatorTest extends TestCase
         $this->assertCount(1, $validator);
         $this->assertNotEmpty($validator);
     }
+
+    public function testSuccessfulUserValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'username' => 'marston97',
+            'email' => 'marston@gmail.com',
+            'password' => 'Marston97!',
+            'password_confirmation' => 'Marston97!'
+        ]);
+
+        $validator = RequestValidator::validateUserRequest($request);
+
+        $this->assertCount(0, $validator);
+        $this->assertEmpty($validator);
+    }
+
+    public function testFailedUserValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'username' => 'marston97',
+            'email' => '',
+            'password' => 'Marston97',
+            'password_confirmation' => 'Marston97'
+        ]);
+
+        $validator = RequestValidator::validateUserRequest($request);
+
+        $this->assertCount(2, $validator);
+        $this->assertNotEmpty($validator);
+    }
+
+    public function testCorrectUserValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'username' => 'marston97',
+            'email' => ' ',
+            'password' => 'Marston97',
+            'password_confirmation' => 'Marston97'
+        ]);
+
+        $expectedValidationMessages = [
+            'email' => ['El campo email es obligatorio.'],
+            'password' => ['La contraseña debe contener al menos una letra mayúscula, una minúscula, un número y un carácter especial.']
+        ];
+
+        $validator = RequestValidator::validateUserRequest($request);
+
+        $this->assertCount(2, $validator);
+        $this->assertNotEmpty($validator);
+        $this->assertEquals($expectedValidationMessages, $validator->messages());
+    }
+
+    public function testSuccessfulEmailValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'email' => 'luiszapata@gmail.com'
+        ]);
+
+        $validator = RequestValidator::validateEmailRequest($request);
+
+        $this->assertCount(0, $validator);
+        $this->assertEmpty($validator);
+    }
+
+    public function testCorrectEmailValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'email' => ''
+        ]);
+
+        $expectedValidationMessages = [
+            'email' => ['El campo email es obligatorio.']
+        ];
+
+        $validator = RequestValidator::validateEmailRequest($request);
+
+        $this->assertCount(1, $validator);
+        $this->assertNotEmpty($validator);
+        $this->assertEquals($expectedValidationMessages, $validator->messages());
+    }
+
+    public function testCorrectSearchValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'search_text' => ''
+        ]);
+
+        $expectedValidationMessages = [
+            'search_text' => ['El campo search text es obligatorio.']
+        ];
+
+        $validator = RequestValidator::validateSearchRequest($request);
+
+        $this->assertCount(1, $validator);
+        $this->assertNotEmpty($validator);
+        $this->assertEquals($expectedValidationMessages, $validator->messages());
+    }
+
+    public function testSuccessfulSearchValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'search_text' => 'Luis Zapata'
+        ]);
+
+        $validator = RequestValidator::validateSearchRequest($request);
+
+        $this->assertCount(0, $validator);
+        $this->assertEmpty($validator);
+    }
+
+    public function testCorrectUserIdValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'user_id' => ''
+        ]);
+
+        $expectedValidationMessages = [
+            'user_id' => ['El campo user id es obligatorio.']
+        ];
+
+        $validator = RequestValidator::validateUserId($request);
+
+        $this->assertCount(1, $validator);
+        $this->assertNotEmpty($validator);
+        $this->assertEquals($expectedValidationMessages, $validator->messages());
+    }
+
+    public function testCorrectPasswordValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'actual_password' => 'Marston97!',
+            'password' => 'Marston97#',
+            'password_confirmation' => 'Marston97$'
+        ]);
+
+        $expectedValidationMessages = [
+            'password_confirmation' => ['Las contraseñas no coinciden.']
+        ];
+
+        $validator = RequestValidator::validatePasswordRequest($request);
+
+        $this->assertCount(1, $validator);
+        $this->assertNotEmpty($validator);
+        $this->assertEquals($expectedValidationMessages, $validator->messages());
+    }
+
+    public function testSuccessfulPasswordValidation(): void
+    {
+        $request = new Request();
+        $request->replace([
+            'actual_password' => 'Marston97!',
+            'password' => 'Marston97#',
+            'password_confirmation' => 'Marston97#'
+        ]);
+
+        $validator = RequestValidator::validatePasswordRequest($request);
+
+        $this->assertCount(0, $validator);
+        $this->assertEmpty($validator);
+    }
+
+
 }
