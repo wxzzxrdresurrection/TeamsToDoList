27a939c864ec0f1c57da168ba72cda0a032ca3d1
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-12-10 22:59:01 -0600

    feat(app): add TeamTasks component to display tasks assigned to users

---
+++ resources/js/Pages/global/TeamTasks.jsx
@@ -0,0 +1,19 @@
+export default function TeamTasks({ tasks }) {
+    return (
+        <div className="container mx-auto my-auto">
+            <div className="flex flex-col mx-4">
+                <div className="flex flex-col">
+                    {tasks.map((task) => (
+                        <div className="border-b border-zinc-700 w-full py-3 hover:cursor-pointer"
+                        key={task.id}>
+                            <p className="text-md">
+                                {task.title} - Asignada a: {task.responsible.username}
+                            </p>
+                            <p className="text-md text-zinc-400"> {task.body}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
